48dae589754940bcb77c63ab5440e23decb35da3
krlpopiel <admir0305@gmail.com>
2025-10-06 11:37:19 +0200

    Zadanie 4 - npm install camelcase - czyli wykorzystanie modułu do obsługi stringów

--- lab1/index.js
+++ lab1/index.js
@@ -1,17 +1,15 @@
-const http = require('http');
-const path = require("path");
-const v8 = require("v8");
-const hostname = '127.0.0.1';
-const port = 8888;
-const server = http.createServer((request, response) => {
- response.statusCode = 200;
- response.setHeader('Content-Type', 'text/plain');
- response.end('Hello, from node.js serwer !\n');
+import camelCase from 'camelcase';
 
-});
-server.listen(port, hostname, () => {
- console.log(v8.getHeapStatistics());
- console.log(path.basename(__filename));
- console.log(path.join(__dirname, 'uploads','images'));
- console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log('Oryginał: piekielnie-Duzo_zNAkow_pODlog_ORAZ-Myslnikow-i-roznych_Wielkosci-_Znakow\n')
+
+console.log(camelCase('Piekielnie-Duzo_zNAkow_pODlog_ORAZ-Myslnikow-i-roznych_Wielkosci-_Znakow\n'))
+console.log('usuwanie "-" oraz "_" z ciagu znakow z pomniejszeniem pierwszej litery i ustandaryzowaniem wielkosci liter\n')
+
+console.log('Modul przydatny podczas obslugiwania stringów np. celem tworzenia ustandaryzowanych nazw')
+console.log('Modul zawiera wiele opcji wypisu\n')
+
+console.log(camelCase('piekielnie-Duzo_zNAkow_pODlog_ORAZ-Myslnikow-i-roznych_Wielkosci-_Znakow\n', {pascalCase: true}))
+console.log('usuwanie "-" oraz "_" z ciagu znakow z powiekszeniem pierwszej litery i ustandaryzowaniem wielkosci liter\n')
+
+console.log(camelCase('Piekielnie-Duzo_zNAkow_pODlog_ORAZ-Myslnikow-i-roznych_Wielkosci-_Znakow\n', {pascalCase: false, preserveConsecutiveUppercase: true}))
+console.log('usuwanie "-" oraz "_" z ciagu znakow z pomniejszeniem pierwszej litery z zachowaniem wielkosci liter\n')
