ebd39f42c4addb2890f668bd363022c5a60915b0
krlpopiel <admir0305@gmail.com>
2025-10-06 11:21:14 +0200

    Zadanie 3 - dodanie nowych modułów (moduł util jest deprecated, więc zastąpiono console)

--- lab1/index.js
+++ lab1/index.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const path = require("path");
+const v8 = require("v8");
 const hostname = '127.0.0.1';
 const port = 8888;
-const server = http.createServer(function(request, response) {
+const server = http.createServer((request, response) => {
  response.statusCode = 200;
  response.setHeader('Content-Type', 'text/plain');
- response.end('Zmienione powitanie :)\n');
+ response.end('Hello, from node.js serwer !\n');
 
 });
 server.listen(port, hostname, () => {
+ console.log(v8.getHeapStatistics());
+ console.log(path.basename(__filename));
+ console.log(path.join(__dirname, 'uploads','images'));
  console.log(`Server running at http://${hostname}:${port}/`);
 });
