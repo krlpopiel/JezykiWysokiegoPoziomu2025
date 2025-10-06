dbcfe8101437d8e4abddbf2dac5d5c6a83c876b0
krlpopiel <admir0305@gmail.com>
2025-10-06 11:06:45 +0200

    Zadanie 2 cz.2

--- lab1/index.js
+++ lab1/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
-const data = fs.readFileSync('package.json');
 console.log("Początek skryptu.");
-console.log(data.toString());
+fs.readFile('package.json', function (err, data) {
+ if (err) return console.error(err);
+ console.log(data.toString());
+});
 console.log("Koniec skryptu.");
