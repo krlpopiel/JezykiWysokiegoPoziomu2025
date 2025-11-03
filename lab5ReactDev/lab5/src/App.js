d4584be1be2c8bce95b6bd57af3e3c51870e12c9
krlpopiel <admir0305@gmail.com>
2025-11-03 10:27:31 +0100

    Zad 2) Ćwiczenie 2

--- lab5ReactDev/lab5/src/App.js
+++ lab5ReactDev/lab5/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import ScoreDisplay from './ScoreDisplay';
 import ToggleDetails from './ToggleDetails';
 
 function App() {
   return (
     <div className="App">
       <ToggleDetails/>
+      <ScoreDisplay score={0}/>
+      <ScoreDisplay score={50}/>
+      <ScoreDisplay score={100}/>
     </div>
   );
 }
