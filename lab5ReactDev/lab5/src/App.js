6b66cfb83a24d040514e0dafc8b7e2df084aa7cc
krlpopiel <admir0305@gmail.com>
2025-11-03 10:24:01 +0100

    Zad 2) Ćwiczenie 1

---
+++ lab5ReactDev/lab5/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToggleDetails from './ToggleDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <ToggleDetails/>
+    </div>
+  );
+}
+
+export default App;
