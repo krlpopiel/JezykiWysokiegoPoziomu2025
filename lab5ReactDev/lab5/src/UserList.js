bc005295032eafe433cf9722cc274be1b8f76df2
krlpopiel <admir0305@gmail.com>
2025-11-03 10:33:53 +0100

    Zad 2) Ćwiczenie 4b - ze spreadem

--- lab5ReactDev/lab5/src/UserList.js
+++ lab5ReactDev/lab5/src/UserList.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+
 function UserList() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
-  }, []);
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((data) => setUsers([...data]));
+    }, []);
+
+    return (
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    );
 }
 export default UserList;
