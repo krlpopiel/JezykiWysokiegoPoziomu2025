09429937a93858ee07949c8be09c371a8af4c1a3
krlpopiel <admir0305@gmail.com>
2025-11-03 10:30:29 +0100

    Zad 2) Ćwiczenie 3

--- lab5ReactDev/lab5/src/App.js
+++ lab5ReactDev/lab5/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ScoreDisplay from './ScoreDisplay';
+import TaskList from './TaskList';
 import ToggleDetails from './ToggleDetails';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <ScoreDisplay score={0}/>
       <ScoreDisplay score={50}/>
       <ScoreDisplay score={100}/>
+      <TaskList></TaskList>
     </div>
   );
 }
