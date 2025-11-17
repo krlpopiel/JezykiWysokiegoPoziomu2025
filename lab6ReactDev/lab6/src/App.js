21725d91fb40d8eed11e566999d41aee75010c5c
krlpopiel <admir0305@gmail.com>
2025-11-17 10:48:39 +0100

    Zad 2) Ćw. 1

--- lab6ReactDev/lab6/src/App.js
+++ lab6ReactDev/lab6/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
+import ClickCounter from './ClickCounter';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <ClickCounter/>
     </div>
   );
 }
