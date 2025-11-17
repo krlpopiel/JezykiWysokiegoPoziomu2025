6957620c146310f72da09d3b76de61f0209a6f3f
krlpopiel <admir0305@gmail.com>
2025-11-17 11:14:05 +0100

    Zad 2) Ćw. 4

--- lab6ReactDev/lab6/src/App.js
+++ lab6ReactDev/lab6/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 import ClickCounter from './ClickCounter';
 import PrimeCalculator from './PrimeCalculator';
 import FormReducer from './FormReducer';
+import { useTheme, ThemeProvider } from "./ThemeContext"
+
+
+function ThemeSwitcher() {
+ const { dark, toggleTheme } = useTheme()
+ return (
+ <div style={{ background: dark ? "#333" : "#fff", color:
+dark ? "#fff" : "#000" }}>
+ <button onClick={toggleTheme}>Przełącz motyw</button>
+ </div>
+ )
+}
 
 function App() {
   return (
@@ -15,6 +26,10 @@ function App() {
       <PrimeCalculator/>
       <hr></hr>
       <FormReducer/>
+      <hr></hr>
+      <ThemeProvider>
+      <ThemeSwitcher/>
+      </ThemeProvider>
     </div>
   );
 }
