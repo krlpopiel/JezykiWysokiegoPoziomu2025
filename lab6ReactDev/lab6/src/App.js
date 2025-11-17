da3b3fd008c8f29dbf62e30215a997854d034b1b
krlpopiel <admir0305@gmail.com>
2025-11-17 10:58:08 +0100

    Zad 2) Ćw. 3

--- lab6ReactDev/lab6/src/App.js
+++ lab6ReactDev/lab6/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Hello from './Hello';
 import ClickCounter from './ClickCounter';
 import PrimeCalculator from './PrimeCalculator';
+import FormReducer from './FormReducer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <hr></hr>
       <PrimeCalculator/>
       <hr></hr>
+      <FormReducer/>
     </div>
   );
 }
